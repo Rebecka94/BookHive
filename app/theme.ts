b118645c9d2a49749a7a5f06d2583bacefa27427
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-12-04 14:20:11 +0100

    updated theme with new text styling

--- app/theme.ts
+++ app/theme.ts
@@ -9,6 +9,7 @@ let theme = createTheme({
     h3: { fontSize: "2rem", lineHeight: 1.35 },
     h4: { fontSize: "1.5rem", lineHeight: 1.4 },
     body1: { fontSize: "1.125rem", lineHeight: 1.6 },
+    body2: {fontSize: "0.7rem", lineheight: 1}
   },
 
   palette: {
@@ -49,6 +50,10 @@ theme = createTheme(theme, {
     body1: {
       [theme.breakpoints.down("md")]: { fontSize: "0.9rem" },
       [theme.breakpoints.down("sm")]: { fontSize: "0.7rem" },
+    },
+    body2: {
+      [theme.breakpoints.down("md")]: { fontSize: "0.7rem" },
+      [theme.breakpoints.down("sm")]: { fontSize: "0.9rem" },
     },
   },
 });
